f848cbb0b544c7c54788fa6e330b046feb206514
akhandelwal1207 <31066895+akhandelwal1207@users.noreply.github.com>
2025-09-09 08:56:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-//test 
+//test 11
 
 // Routes
 app.get('/', (req, res) => {
